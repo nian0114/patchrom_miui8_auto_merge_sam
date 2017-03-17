8ff0b3548f4333977a419089453cf14c4951f194
nian0114 <268078545@qq.com>
2017-03-18 01:32:33 +0800

    change iget to sget

---
+++ customize_framework.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# $1: dir for miui
+# $2: dir for original
+
+APKTOOL="$PORT_ROOT/tools/apktool --quiet"
+GIT_APPLY=$PORT_ROOT/tools/git.apply
+BUILD_OUT=out
+
+function appendSmaliPart() {
+  	cd overlay
+    for file in `find $1/smali -name *.part`
+    do
+        filepath=`dirname $file`
+        filename=`basename $file .part`
+        dstfile="../out/$filepath/$filename"
+        cat $file >> $dstfile
+    done
+	  cd ..
+}
+
+function overlaySmali() {
+    for file in `find $1/smali -name *.smali`
+    do
+        filepath=`dirname $file`
+        cp -f $file out/$filepath
+    done
+}
+
+function applyPatch() {
+	for file in $1/*.patch
+	do
+		cp $file out/
+		cd out
+		git.apply `basename $file`
+        for file2 in `find $2 -name *.rej`
+        do
+            echo "$file2 fail"
+            exit 1
+        done
+		cd ..
+	done
+}
+
+if [ $2 = "$BUILD_OUT/services" ]
+then
+    find $2/smali/com/android/server/pm/ -type f |xargs sed -i "s#iget-object \(v[0-9]*\), [pv][0-9]*, Lcom/android/server/pm/PackageManagerService;->mPackages:Landroid/util/ArrayMap;#sget-object \1, Lcom/android/server/pm/PackageManagerService;->mPackages:Landroid/util/ArrayMap;#g"
+fi
