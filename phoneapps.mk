ca327be465ca2f6c121ae09d57703ed813030aad
nian0114 <268078545@qq.com>
2017-03-18 00:04:54 +0800

    add makefile

---
+++ phoneapps.mk
@@ -0,0 +1,16 @@
+
+local-phone-apps := NfcNci SPrintSpooler6 PacProcessor CertInstaller KeyChain mcRegistry \
+	Stk UserDictionaryProvider Bluetooth BluetoothMidiService \
+	SLocation ClipboardSaveService ClipboardUIService EasyOneHand3 \
+	FilterInstaller FilterManager FilterProvider \
+	minimode-res PopupuiReceiver_M \
+	TimeService WfdBroker WebViewGoogle \
+	AntHalService ANTPlusPlugins ANTPlusTest ANTRadioService ApexService \
+	com.qualcomm.qti.services.secureui com.quicinc.wbcserviceapp imsservice \
+	ImsSettings ImsTelephonyService
+
+
+local-phone-priv-apps := Shell ProxyHandler HealthService ExternalStorageProvider \
+	SharedStorageBackup InputDevices BackupRestoreConfirmation NSFusedLocation_Global SimSettingMgr \
+	MtpApplication SecSettingsProvider2 SecMediaProvider ShootingModeProvider Alipay_Service AODService \
+	FingerprintService2 OfflineNetworkLocation_Baidu
