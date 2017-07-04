3cf457b5ff6e9ca56d8377aa2380114358eda60a
nian0114 <268078545@qq.com>
2017-07-04 17:53:16 +0800

    use TGP kernel

--- releasetools.py
+++ releasetools.py
@@ -16,20 +16,6 @@ def RemoveDeviceGetprop(info):
       edify.script[i] = ''
       return
 
-def AddArgsForFormatSystem(info):
-  edify = info.script
-  for i in xrange(len(edify.script)):
-    if "mount(" in edify.script[i] and "system" in edify.script[i]:
-      edify.script[i] = 'mount("ext4", "EMMC", "/dev/block/bootdevice/by-name/system", "/system");'
-      return
-
-def AddArgsForFormatData(info):
-  edify = info.script
-  for i in xrange(len(edify.script)):
-    if "mount(" in edify.script[i] and "userdata" in edify.script[i]:
-      edify.script[i] = 'mount("ext4", "EMMC", "/dev/block/bootdevice/by-name/userdata", "/data");'
-      return
-
 def RemoveBootImage(info):
   edify = info.script
   for i in xrange(len(edify.script)):
@@ -52,8 +38,8 @@ def AddBootScripts(info):
     for i in xrange(len(edify.script)):
         if ");" in edify.script[i] and 'show_progress(0.050000, 5);' in edify.script[i] :
 			edify.script[i] += '''
-ifelse(is_substring("G9350", getprop("ro.bootloader")), package_extract_file("devices/g9350/boot.img", "/dev/block/bootdevice/by-name/boot"));
-ifelse(is_substring("G9300", getprop("ro.bootloader")), package_extract_file("devices/g9300/boot.img", "/dev/block/bootdevice/by-name/boot"));'''
+ifelse(is_substring("G935", getprop("ro.bootloader")), package_extract_file("devices/g935x/boot.img", "/dev/block/platform/155a0000.ufs/by-name/BOOT"));
+ifelse(is_substring("G930", getprop("ro.bootloader")), package_extract_file("devices/g930x/boot.img", "/dev/block/platform/155a0000.ufs/by-name/BOOT"));'''
     return
 
 def AddAdditionsApp(info):
@@ -66,6 +52,8 @@ def AddAdditionsApp(info):
 def FullOTA_InstallEnd(info):
     RemoveDeviceAssert(info)
     RemoveDeviceGetprop(info)
+    RemoveBootImage(info)
+    AddBootScripts(info)
 
 def IncrementalOTA_InstallEnd(info):
     RemoveDeviceAssert(info)
