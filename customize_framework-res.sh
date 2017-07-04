e58ae97dd6e9bb80d66b7dbd149a59541863019f
nian0114 <268078545@qq.com>
2017-07-04 16:38:12 +0800

    fix icon size

---
+++ customize_framework-res.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -f $2/drawable-nodpi-v4/default_wallpaper.jpg
+rm -f $2/drawable-sw320dp-hdpi-v13/default_wallpaper.png
+rm -f $2/drawable-sw360dp-xxhdpi-v13/default_wallpaper.png
+rm -f $2/drawable-sw360dp-xxxhdpi-v13/default_wallpaper.png
+rm -f $2/layout-xxxhdpi-v4/notification*.xml
